6376201f8f99595494e6400cd2bee05e04605203
Thomas Schank <DrTom@schank.ch>
2015-07-16 07:07:00 +0200

    1.2.0 Improve thrown/stringify for nested exceptions

--- src/drtom/logbug/thrown.clj
+++ src/drtom/logbug/thrown.clj
@@ -1,7 +1,7 @@
 ; Copyright © 2013 - 2015 Thomas Schank <DrTom@schank.ch>
 
 (ns drtom.logbug.thrown
-  (:require 
+  (:require
     [clojure.stacktrace :as stacktrace]
     [clojure.tools.logging :as logging]
     [clojure.string :as string]
@@ -28,23 +28,29 @@
 ;(reset-ns-filter-regex #".*drtom.*logbug.*")
 
 (defn filter-trace-string-seq [ex-seq filter-regex]
-  (filter 
+  (filter
     #(re-matches filter-regex %)
     ex-seq))
 
-(defn stringify 
+(defn stringify
   ([^Throwable tr]
    (stringify tr *ns-filter-regex* ", "))
   ([^Throwable tr filter-regex join-str]
-   (let [this-tr-str (string/join join-str 
-                                  (map 
+   (let [this-tr-str (string/join join-str
+                                  (map
                                     (fn [ex]
                                       (logging/debug {:ex ex})
                                       (str [(with-out-str (stacktrace/print-throwable ex))
                                             (filter-trace-string-seq (trace-string-seq ex) filter-regex)]))
                                     (expand-to-seq tr)))]
-     (if-let [cause (try (.getNextException tr) (catch Exception _ nil))]
-       (string/join join-str [this-tr-str (stringify cause join-str)])
-       this-tr-str))))
+     (string/join join-str (flatten (conj [this-tr-str]
+                                          (map #(stringify % filter-regex join-str)
+                                               (try (iterator-seq (.iterator tr))
+                                                    (catch Exception _ [])))
+                                          (try [(stringify (.getCause tr))]
+                                               (catch Exception _ []))))))))
+
 
 ;(stringify  (IllegalStateException. "Just a demo"))
+
+;(println (stringify (ex-info "Some error "{:x 42}  (IllegalStateException. "The cause"))))
